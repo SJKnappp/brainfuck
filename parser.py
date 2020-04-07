3d6585c96cac40201ab586016a8f42a06f0f5e34
Samuel Knapp <samuel.j.knapp@btinternet.com>
2020-04-07 17:05:19 +0100

    got the print function . to work as expected

--- parser.py
+++ parser.py
@@ -2,10 +2,16 @@
 def parser(source):
 
 	open=0
+	pos=0
 
 	for char in source:
-		if(char=='<'): pass
-		elif(char=='>'): pass
+		if(char=='<'): 
+			pos-=1
+			if(pos<0): 
+				print('unable to go tp mem address < 0')
+				quit
+
+		elif(char=='>'): pos+=1
 		elif(char=='+'): pass
 		elif(char=='-'): pass
 		elif(char=='.'): pass
@@ -14,6 +20,7 @@ def parser(source):
 		elif(char==']'):
 			if(open>0):
 				open-=1
+				
 
 	if (open!=0):
 		print(f'there are {open} open brackets')
